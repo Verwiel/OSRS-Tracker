87fceff3f740124ab4e0471dd1ef3cb13b552c8e
Drew Verwiel <verwiel.code@gmail.com>
2023-01-18 18:15:27 -0700

    scraper crawl methd, may switch to a python crawler

--- osrs-webcrawler/src/index.ts
+++ osrs-webcrawler/src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world!')
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+const crawl = async (url: string) => {
+  try {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    // let rows = []
+    const links = $("#mw-content-text > div.mw-parser-output > table:nth-child(6) > tbody > tr:nth-child(1) > td:nth-child(3)").text()
+    // const links = $("tbody")
+    //   .map((i, tr) => console.log(tr.children.href))
+    //   .get()
+    console.log(links)
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// crawl("https://oldschool.runescape.wiki/w/Quests/List")
