5e47688fe9a151501f2f2fd328b08098d336b991
Drew Verwiel <verwiel.code@gmail.com>
2023-01-25 10:51:23 -0700

    added more details to the quest modal

--- osrs-tracker/src/components/QuestModal.tsx
+++ osrs-tracker/src/components/QuestModal.tsx
@@ -17,6 +17,7 @@ const customStyles = {
 
 export const QuestModal = () => {
   const { modalOpen, closeModal, selectedQuest } = useQuestCtx()
+  const { link, name, length, series, startPoint, releaseDate, difficulty, description, requirements, itemsRequired, recommended, enemies } = selectedQuest
 
   return (
     <div>
@@ -26,7 +27,75 @@ export const QuestModal = () => {
         style={customStyles}
         contentLabel="Example Modal"
       >
-        <h2 style={{color: 'black'}}>{selectedQuest.name}</h2>
+        <header style={{color: 'black', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+          <span>
+            <h2 style={{color: 'black', margin: '0'}}>{name}</h2>
+          </span>
+          {!series.includes('None') &&
+            <p>Series: {series}</p>
+          }
+          <p>Released: {releaseDate}</p>
+        </header>
+        <table style={{color: 'black'}}>
+          <tbody>
+            <tr>
+              <th>Start Point</th>
+              <td>{startPoint}</td>
+            </tr>
+            <tr>
+              <th>Difficulty</th>
+              <td>{difficulty}</td>
+            </tr>
+            <tr>
+              <th>Description</th>
+              <td>{description}</td>
+            </tr>
+            <tr>
+              <th>Length</th>
+              <td>{length}</td>
+            </tr>
+            <tr>
+              <th>Requirements</th>
+              <td>
+                  {requirements.length > 0 ? 
+                    <ul>
+                      {requirements.map(requirement => (
+                        <li key={requirement}>{requirement}</li>
+                      ))}
+                    </ul>
+                    :
+                    "None"
+                  }
+              </td>
+            </tr>
+            <tr>
+              <th>Items required</th>
+              <td>
+                <ul>
+                  {itemsRequired.map(item => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <th>Recommended</th>
+              <td>
+                <ul>
+                  {recommended.map(item => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <th>Enemies to defeat</th>
+              <td>{enemies}</td>
+            </tr>
+          </tbody>
+        </table>
+        <a  style={{color: 'black'}} href={link} rel="noopener noreferrer" target="_blank" >View the guide on the OSRS wiki</a>
+        <br />
         <button onClick={closeModal}>close</button>
       </Modal>
     </div>
