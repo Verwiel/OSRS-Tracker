295e53944a90c509c73cf41dd89aa02172c4f8ff
Drew Verwiel <verwiel.code@gmail.com>
2023-01-26 14:11:04 -0700

    all nav tabs show

--- osrs-tracker/src/components/navigation/desktop/NavBarTabs.tsx
+++ osrs-tracker/src/components/navigation/desktop/NavBarTabs.tsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { useCharacterCtx } from '../../../context/CharacterProvider';
 import { NavBarTab } from "./NavBarTab";
 
 export const NavBarTabs: React.FC = () => {
-  const { storedUsername } = useCharacterCtx()
   return (
     <div className="nav-bar__tabs">
-      {storedUsername &&
-        <>
-          <NavBarTab path="/levels" label="Levels" />
-          <NavBarTab path="/activities" label="Activities" />
-          <NavBarTab path="/highscores" label="Highscores" />
-        </>
-      }
+      <NavBarTab path="/levels" label="Levels" />
+      <NavBarTab path="/activities" label="Activities" />
+      <NavBarTab path="/highscores" label="Highscores" />
       <NavBarTab path="/quests" label="Quests" />
     </div>
   );
--- osrs-tracker/src/components/navigation/mobile/MobileNavBarTabs.tsx
+++ osrs-tracker/src/components/navigation/mobile/MobileNavBarTabs.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useCharacterCtx } from '../../../context/CharacterProvider';
 import { MobileNavBarTab } from "./MobileNavBarTab";
 
 interface MobileNavBarTabsProps {
@@ -7,17 +6,11 @@ interface MobileNavBarTabsProps {
 }
 
 export const MobileNavBarTabs: React.FC<MobileNavBarTabsProps> = ({ handleClick }) => {
-  const { storedUsername } = useCharacterCtx()
-  
   return (
     <div className="mobile-nav-bar__tabs">
-      {storedUsername &&
-        <>
-          <MobileNavBarTab path="/levels" label="Levels" handleClick={handleClick} />
-          <MobileNavBarTab path="/activities" label="Activities" handleClick={handleClick} />
-          <MobileNavBarTab path="/highscores" label="Highscores" handleClick={handleClick} />
-        </>
-      }
+      <MobileNavBarTab path="/levels" label="Levels" handleClick={handleClick} />
+      <MobileNavBarTab path="/activities" label="Activities" handleClick={handleClick} />
+      <MobileNavBarTab path="/highscores" label="Highscores" handleClick={handleClick} />
       <MobileNavBarTab
         path="/quests"
         label="Quests"
