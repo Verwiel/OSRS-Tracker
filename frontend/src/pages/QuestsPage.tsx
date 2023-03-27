9aceef858ac63c5b43089ec27ae69924c99b3fe6
Drew Verwiel <verwiel.code@gmail.com>
2023-03-27 08:36:57 -0600

    added page loader instead of loading text on Quest List

--- frontend/src/pages/QuestsPage.tsx
+++ frontend/src/pages/QuestsPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useQuestCtx } from "../context/QuestProvider";
 import { PageLayout } from "../components/PageLayout";
 import { QuestList } from "../components/QuestList";
+import { PageLoader } from "../components/PageLoader";
 
 export const QuestsPage: React.FC = () => {
   const { questsLoaded } = useQuestCtx()
@@ -15,7 +16,7 @@ export const QuestsPage: React.FC = () => {
       {questsLoaded ?
         <QuestList />
         :
-        <h2>Loading...</h2>
+        <PageLoader />
       }
     </PageLayout>
   )
